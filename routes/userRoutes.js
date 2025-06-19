4d575b58740b26196bef20401d9e5f56ccb19a01
Gayathri-051998 <gayathrivelayutham52@gmail.com>
2025-06-19 09:40:54 +0530

    user router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,11 @@
-// import express module
 const express = require('express');
 const { register, login, logout, me } = require('../controllers/userController');
 const { isAuthenticated } = require('../middlewares/auth');
+const router = express.Router();
 
-// create a router
-const userRouter = express.Router();
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', isAuthenticated, logout);
+router.get('/me', isAuthenticated, me);
 
-userRouter.post('/register',register);
-userRouter.post('/login', login);
-userRouter.post('/logout',isAuthenticated,logout);
-userRouter.get('/me',isAuthenticated, me);
-
-// export the router
-
-
-module.exports = userRouter;
+module.exports = router;
